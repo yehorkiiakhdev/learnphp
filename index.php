000f62f7733f4f16c81daa969ff4ad1598ae5a2c
Yehor Kiiakh <yehor.kiiakh@tptlive.ee>
2026-09-11 09:59:40 +0300

    Learning functions and loops

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-    ?name = "Yehor"
-    ?age = 17
-    ?emojy = "👌"
+    function hello() {
+        var_dump("Hello!");
+    }
+
+    hello();
+    hello();
+
+    function square(5a) {
+        if ($a<0){
+            retunr 0;
+        }
+        return $a * $a;
+        var_dump('asdasdads')
+    }
+
+    $answer = square(4);
+    var_dump($answer);
+    var_dump(square(5));
+
 ?>
